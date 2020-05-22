1c3d85960fcfa70241d513da018dc6107fff38cc
Dani ONeal <droneal11015@gmail.com>
2020-05-22 09:57:54 -0400

    init .net app

---
+++ number-guesser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+//function
+namespace number_guesser
+{
+    //main class
+    class Program
+    {
+        //entry point method
+        static void Main(string[] args)
+        {
+            //start here
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
