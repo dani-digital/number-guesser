3ab964fd21d3152762bb5058ca984951cecb78de
Dani ONeal <droneal11015@gmail.com>
2020-05-22 10:20:24 -0400

    saving current version

--- number-guesser/Program.cs
+++ number-guesser/Program.cs
@@ -10,8 +10,12 @@ namespace number_guesser
         static void Main(string[] args)
         {
             string name = "Dani ONeal";
+            int age = 29;
+            string loc = "Gray, GA";
             //start here
-            Console.WriteLine(name);
+            //Console.WriteLine("Hello, " +name); //concat like JS
+            //more efficient
+            Console.WriteLine("{0} is {1} and lives in {2}.", name, age, loc);
         }
     }
 }
