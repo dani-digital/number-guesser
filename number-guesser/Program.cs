ec191889acf106f59e94ea1490c0dc49ace607d8
Dani ONeal <droneal11015@gmail.com>
2020-05-22 10:22:58 -0400

    added app header

--- number-guesser/Program.cs
+++ number-guesser/Program.cs
@@ -9,13 +9,12 @@ namespace number_guesser
         //entry point method
         static void Main(string[] args)
         {
-            string name = "Dani ONeal";
-            int age = 29;
-            string loc = "Gray, GA";
-            //start here
-            //Console.WriteLine("Hello, " +name); //concat like JS
-            //more efficient
-            Console.WriteLine("{0} is {1} and lives in {2}.", name, age, loc);
+            //set app variables
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Dani O'Neal";
+            //header
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
         }
     }
 }
